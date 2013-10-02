6f0ad02ac919a5c1a91d03f6abbb728403d151c8
Philipp Hansch <dev@phansch.net>
2013-10-02 13:49:41 +0200

    Added tokenizer

--- 12_rpn_calculator/rpn_calculator.rb
+++ 12_rpn_calculator/rpn_calculator.rb
@@ -35,4 +35,15 @@ class RPNCalculator
     @value = nums[index_a].to_f.send(op, nums[index_b].to_f) # Object.send
     push(@value)
   end
+
+  def tokens(string)
+    tokens = string.split(' ')
+    tokens.each_index do |i|
+      if %W(+ - * /).include?(tokens[i])
+        tokens[i] = tokens[i].to_sym
+      end
+      tokens[i] = Integer(tokens[i]) unless %i(+ - * /).include?(tokens[i])
+
+    end
+  end
 end
